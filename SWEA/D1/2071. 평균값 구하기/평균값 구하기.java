a24defcf1339a07ba5843926a6c3d7f6a03b1495
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-07 15:04:55 +0900

    [D1] Title: 평균값 구하기, Time: 134 ms, Memory: 20,092 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			int sum = 0;
+			for (int i = 0; i < 10; i++) sum += sc.nextInt();
+			if (sum % 10 >= 5) sum += 5;
+			System.out.printf("#%d %d\n", t, sum/10);
+		}
+	}
+}
