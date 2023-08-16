d1be59a2a164ca071cb3db6f96367c4c3a5221ef
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-16 11:40:47 +0900

    [D3] Title: 제로, Time: 505 ms, Memory: 97,888 KB -BaekjoonHub

--- SWEA/D3/8931. 제로/제로.java
+++ SWEA/D3/8931. 제로/제로.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Solution {
 	public static void main(String[] args) {
@@ -7,14 +6,15 @@ public class Solution {
 		int T = sc.nextInt();
 		for (int t = 1; t <= T; t++) {
 			int K = sc.nextInt();
-			Stack<Integer> stack = new Stack<>();
+			int[] arr = new int[K];
+			int cur = 0;
 			for (int k = 0; k < K; k++) {
 				int tmp = sc.nextInt();
-				if (tmp == 0) stack.pop();
-				else stack.push(tmp);
+				if (tmp == 0) cur--;
+				else arr[cur++] = tmp;
 			}
 			int sum = 0;
-			for (int num : stack) sum += num;
+			for (int i = 0; i < cur; i++) sum += arr[i];
 			System.out.printf("#%d %d\n", t, sum);
 		}
 	}
