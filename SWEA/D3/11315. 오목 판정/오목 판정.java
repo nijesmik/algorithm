8959f040bd168a46292e3a0836f308d7734b01fc
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-21 01:09:24 +0900

    [D3] Title: 오목 판정, Time: 174 ms, Memory: 24,908 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
 public class Solution {
-	static int[][] board;
+	static char[][] board;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int t = 1; t <= T; t++) {
 			int n = sc.nextInt();
-			board = new int[n][n];
-			for (int i = 0; i < n; i++) {
-				String[] tmp = sc.next().split("");
-				for (int j = 0; j < n; j++)
-					board[i][j] = tmp[j].equals(".") ? 0 : 1;
-			}
+			board = new char[n][];
+			for (int i = 0; i < n; i++) board[i] = sc.next().toCharArray();
 			System.out.printf("#%d %s\n", t, check(n));
 		}
 	}
@@ -22,11 +18,11 @@ public class Solution {
 			for (int j = 0; j < n; j++) {
 				int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
 				for (int k = 0; k < 5; k++) {
-					if (j+k < n) sum1 += board[i][j+k];
+					if (j+k < n && board[i][j+k] == 'o') sum1++;
 					if (i+k < n) {
-						sum2 += board[i+k][j];
-						if (j+k < n) sum3 += board[i+k][j+k];
-						if (0 <= j-k) sum4 += board[i+k][j-k];
+						if (board[i+k][j] == 'o') sum2++;
+						if (j+k < n && board[i+k][j+k] == 'o') sum3++;
+						if (0 <= j-k && board[i+k][j-k] == 'o') sum4++;
 					}
 				}
 				if (sum1 == 5 || sum2 == 5 || sum3 == 5 || sum4 == 5)
