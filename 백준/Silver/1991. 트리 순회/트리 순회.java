74fc35bc124010410e13461596bd8da506c8c018
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-23 09:19:04 +0900

    [Silver I] Title: 트리 순회, Time: 116 ms, Memory: 12888 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,49 +1,30 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 class Main {
+	static char[][] tree = new char[26][2];
 	static String[] ans = new String[3];
-    static class Node {
-		Node left;
-		Node right;
-		char data;
-
-		Node(char data) {
-			this.data = data;
-		}
-	}
 
 	public static void main(String[] args) {
 		ans[0] = ""; ans[1] = ""; ans[2] = "";
-		HashMap<Character, Node> hashMap = new HashMap<>();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		Node root = new Node('A');
-		hashMap.put('A', root);
 		for (int i = 0; i < n; i++) {
-			Node node = hashMap.get(sc.next().charAt(0));
-			char a = sc.next().charAt(0);
-			if (a != '.') {
-				node.left = new Node(a);
-				hashMap.put(a, node.left);
-			}
-			char b = sc.next().charAt(0);
-			if (b != '.') {
-				node.right = new Node(b);
-				hashMap.put(b, node.right);
-			}
+			int idx = sc.next().charAt(0) - 'A';
+			tree[idx][0] = sc.next().charAt(0);
+			tree[idx][1] = sc.next().charAt(0);
 		}
-		getAns(root);
+		getAns('A');
 		System.out.println(ans[0]);
 		System.out.println(ans[1]);
 		System.out.println(ans[2]);
 	}
 
-	static void getAns(Node node) {
-		ans[0] += node.data; 
-		if (node.left != null) getAns(node.left);
-		ans[1] += node.data; 
-		if (node.right != null) getAns(node.right);
-		ans[2] += node.data; 
+	static void getAns(char c) {
+		if (c == '.') return;
+		ans[0] += c; 
+		getAns(tree[c - 'A'][0]);
+		ans[1] += c; 
+		getAns(tree[c - 'A'][1]);
+		ans[2] += c; 
 	}
 }
