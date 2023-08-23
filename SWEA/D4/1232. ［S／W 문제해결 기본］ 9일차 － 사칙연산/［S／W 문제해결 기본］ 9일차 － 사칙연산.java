b3debb16100db3b3a300607f214cecce9abb0d71
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-23 21:51:46 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 198 ms, Memory: 36,872 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
@@ -2,29 +2,30 @@ import java.util.Scanner;
 
 public class Solution {
 	static String[] data;
-	static int[][] link;
+	static int[] left, right;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for (int t = 1; t <= 10; t++) {
 			int n = Integer.parseInt(sc.nextLine());
 			data = new String[n+1];
-			link = new int[n+1][2];
+			left = new int[n+1];
+			right = new int[n+1];
 			for (int i = 1; i <= n; i++) {
 				String[] tmp = sc.nextLine().split(" ");
 				data[i] = tmp[1];
 				if (tmp.length == 4) {
-                    link[i][0] = Integer.parseInt(tmp[2]);
-                	link[i][1] = Integer.parseInt(tmp[3]);
+                    left[i] = Integer.parseInt(tmp[2]);
+                	right[i] = Integer.parseInt(tmp[3]);
                 }
 			}
 			System.out.printf("#%d %d\n", t, calc(1));
 		}
 	}
 	static int calc(int idx) {
-		if (link[idx][0] + link[idx][1] == 0) 
+		if (left[idx] + right[idx] == 0) 
 			return Integer.parseInt(data[idx]);
-		int a = calc(link[idx][0]);
-		int b = calc(link[idx][1]);
+		int a = calc(left[idx]);
+		int b = calc(right[idx]);
         char c = data[idx].charAt(0);
 		if (c == '+') return a+b;
 		else if (c == '-') return a-b;
