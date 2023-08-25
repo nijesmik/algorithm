ad0af080701df83a1a505e86dc82d3b0886c491e
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-25 20:09:37 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 162 ms, Memory: 22,600 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -20,30 +20,22 @@ public class Solution {
 		int min = n*m;
 		for (int w = 1; w <= n-2; w++) {
 			for (int b = 1; b <= n-2; b++) {
-				for (int r = 1; r <= n-2; r++) {
-					if (w + b + r != n) continue;
-					min = Math.min(color(w, b, r), min);
- 				}
+				if (w + b + 1 <= n)
+					min = Math.min(paint(w, b), min);
 			}
 		}
 		return min;
 	}
 
-	static int color(int w, int b, int r) {
+	static int paint(int w, int b) {
 		int cnt = 0;
-		for (int i = 0; i < w; i++) {
+		char paint;
+		for (int i = 0; i < n; i++) {
+			if (i < w) paint = 'W';
+			else if (i < w+b) paint = 'B';
+			else paint = 'R';
 			for (int j = 0; j < m; j++) {
-				if (board[i][j] != 'W') cnt++;
-			}
-		}
-		for (int i = w; i < w+b; i++) {
-			for (int j = 0; j < m; j++) {
-				if (board[i][j] != 'B') cnt++;
-			}
-		}
-		for (int i = w+b; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (board[i][j] != 'R') cnt++;
+				if (board[i][j] != paint) cnt++;
 			}
 		}
 		return cnt;
