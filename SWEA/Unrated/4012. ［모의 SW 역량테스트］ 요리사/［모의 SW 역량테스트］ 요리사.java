fefb2c37a614d9c80120879d143ce8d1f60c5611
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-08-31 17:50:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 286 ms, Memory: 39,728 KB -BaekjoonHub

---
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Solution {
+	static int[][] synergy;
+	static int min, r, n;
+	static boolean[] select;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for (int t = 1; t <= tc; t++) {
+			n = sc.nextInt();
+			synergy = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++)
+					synergy[i][j] = sc.nextInt();
+			}
+			r = n / 2;
+			min = 20000 * r;
+			select = new boolean[n];
+			getAnswer(0, 0);
+			System.out.printf("#%d %d\n", t, min);
+		}
+	}
+
+	static void getAnswer(int idx, int sidx) {
+		if (sidx == r) {
+			min = Math.min(min, getSynergy());
+			return;
+		}
+		if (idx == n)
+			return;
+		select[idx] = true;
+		getAnswer(idx + 1, sidx + 1);
+		select[idx] = false;
+		getAnswer(idx + 1, sidx);
+	}
+
+	static int getSynergy() {
+		int synergy1 = 0, synergy2 = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (select[i] && select[j]) synergy1 += synergy[i][j];
+				if (!select[i] && !select[j]) synergy2 += synergy[i][j];
+			}
+		}
+		return Math.abs(synergy1 - synergy2);
+	}
+}
