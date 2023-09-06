c56f0325e0c912313b933104a6797ab3a3c4d32e
nijesmik <bkybb@duck.com>
2023-09-06 10:13:10 +0900

    ♻️ Refactor : [Silver IV] 동전 0

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -5,11 +5,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(), k = sc.nextInt(), cnt = 0;
 		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
-		for (int i = n-1; i >= 0; i--) {
-			if (k >= arr[i]) {
-				cnt += k/arr[i];
-				k %= arr[i];
+		while (n-- > 0) arr[n] = sc.nextInt();
+		for (int i : arr) {
+			if (k >= i) {
+				cnt += k/i;
+				k %= i;
 			}
 		}
 		System.out.println(cnt);
