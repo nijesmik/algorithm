20b580eb0d219ca836b438223a24d173f76debfb
nijesmik <128948538+nijesmik@users.noreply.github.com>
2023-09-18 12:23:33 +0900

    [Silver II] Title: 과자 나눠주기, Time: 2220 ms, Memory: 191024 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static Integer[] snacks;
+	static int n, m;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		snacks = new Integer[m];
+		for (int i = 0; i < m; i++)
+			snacks[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(snacks);
+		int start = 1, end = snacks[m-1], result = 0;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (check(mid)) {
+				result = mid;
+				start = mid + 1;
+			}
+			else end = mid - 1;
+		}
+		System.out.println(result);
+	}
+	static boolean check(int cmp) {
+		int cnt = 0;
+		for (int i = 0; i < n && i < m; i++) {
+			cnt += snacks[m-1-i] / cmp;
+			if (cnt >= n) return true;
+		}
+		return false;
+	}
+}
