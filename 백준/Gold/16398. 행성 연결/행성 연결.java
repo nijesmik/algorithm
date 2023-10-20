00afdb808de9396c722dbfaa0635344fa9b181c0
kimsejin <128948538+nijesmik@users.noreply.github.com>
2023-10-20 10:40:54 +0900

    [Gold IV] Title: 행성 연결, Time: 1748 ms, Memory: 310000 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+	static class Node implements Comparable<Node> {
+		int idx, cost;
+		Node(int i, int c) {
+			idx = i; cost = c;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] cost = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cost[i][j] = sc.nextInt();
+			}
+		}
+		boolean[] visit = new boolean[n];
+		PriorityQueue<Node> q = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			q.add(new Node(i, cost[0][i]));
+		}
+		visit[0] = true;
+		int cnt = 1;
+		long total = 0;
+		while (cnt < n) {
+			Node cur = q.poll();
+			if (visit[cur.idx]) continue;
+			visit[cur.idx] = true;
+			cnt++;
+			total += cur.cost;
+			for (int i = 0; i < n; i++) {
+				q.add(new Node(i, cost[cur.idx][i]));
+			}
+		}
+		System.out.println(total);
+	}
+}
