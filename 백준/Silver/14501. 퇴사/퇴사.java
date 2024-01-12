5dc2a38cbd4be59f39ba7ea1d43eb8326f6c4308
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-01-12 13:34:23 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -7,26 +7,18 @@ public class Main {
         int n = sc.nextInt();
         int[] time = new int[n];
         int[] pay = new int[n];
-        int[] dpPay = new int[n];
+        int[] dpPay = new int[n+1];
         for (int i = 0; i < n; i++) {
             time[i] = sc.nextInt();
             pay[i] = sc.nextInt();
         }
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int curPay = 0;
+        int i = n;
+        while (i-- > 0) {
             if (i+time[i] <= n) {
-                curPay = pay[i];
+                dpPay[i] = dpPay[i+time[i]] + pay[i];
             }
-            for (int j = 0; j <= i; j++) {
-                int totalPay = curPay;
-                if (j+time[j] <= i) {
-                    totalPay += dpPay[j];
-                }
-                dpPay[i] = Math.max(dpPay[i], totalPay);
-            }
-            max = Math.max(max, dpPay[i]);
+            dpPay[i] = Math.max(dpPay[i], dpPay[i+1]);
         }
-        System.out.println(max);
+        System.out.println(dpPay[0]);
     }
 }
