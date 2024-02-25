1e24bcc0d7b2f091a9e26941ac3350b3f084be46
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-02-25 13:16:00 +0900

    [Silver II] Title: 아기 상어 2, Time: 328 ms, Memory: 18996 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,56 +1,41 @@
+
 import java.util.*;
 
 public class Main {
     static int sizeR, sizeC, map[][], ans;
+    static List<Node> sharks;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sizeR = sc.nextInt();
         sizeC = sc.nextInt();
         map = new int[sizeR][sizeC];
+        sharks = new ArrayList<>();
         for (int i = 0; i < sizeR; i++) {
             for (int j = 0; j < sizeC; j++) {
                 map[i][j] = sc.nextInt();
+                if (map[i][j] == 1) {
+                    sharks.add(new Node(i, j));
+                }
             }
         }
         for (int i = 0; i < sizeR; i++) {
             for (int j = 0; j < sizeC; j++) {
-                bfs(i, j);
+                if (map[i][j] == 0) {
+                    getDist(i, j);
+                }
             }
         }
         System.out.println(ans);
     }
 
-    static int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 }, dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-    static void bfs(int r, int c) {
-        if (map[r][c] == 1) {
-            return;
+    static void getDist(int r, int c) {
+        int min = Math.max(sizeR, sizeC);
+        for (Node shark : sharks) {
+            int dist = Math.max(Math.abs(shark.r - r), Math.abs(shark.c - c));
+            min = Math.min(min, dist);
         }
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(r, c));
-        boolean[][] visited = new boolean[sizeR][sizeC];
-        visited[r][c] = true;
-        int dist = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                Node cur = q.poll();
-                for (int i = 0; i < 8; i++) {
-                    int nr = cur.r + dr[i], nc = cur.c + dc[i];
-                    if (nr < 0 || nr >= sizeR || nc < 0 || nc >= sizeC || visited[nr][nc]) {
-                        continue;
-                    }
-                    if (map[nr][nc] == 1) {
-                        ans = Math.max(ans, dist);
-                        return;
-                    }
-                    visited[nr][nc] = true;
-                    q.add(new Node(nr, nc));
-                }
-            }
-            dist++;
-        }
+        ans = Math.max(ans, min);
     }
 
     static class Node {
