55fb5c543c890dba31450e3b493888aae609d655
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-02-26 00:07:17 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 140 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -37,11 +37,9 @@ public class Main {
         int score = 0;
         for (int i = sizeR; i > 0; i--) {
             int[][] enemies = new int[sizeC][];
-            for (int d = 1; d <= sizeD; d++) {
-                for (int j : permutation) {
-                    if (enemies[j] == null) {
-                        enemies[j] = kill(i, j, d);
-                    }
+             for (int j : permutation) {
+                for (int d = 1; d <= sizeD && enemies[j] == null; d++) {
+                    enemies[j] = kill(i, j, d);
                 }
             }
             for (int[] e : enemies) {
