a2c969646d7dccc97f8f4e0dd6ffe304a7de8e8b
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-03-09 21:16:08 +0900

    [Silver I] Title: 1로 만들기 2, Time: 128 ms, Memory: 16852 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -9,11 +9,11 @@ public class Main {
 		dp[3] = 1;
 		for (int i = 4; i <= n; i++) {
 			dp[i] = dp[i - 1];
-			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 2]);
+			if (i % 2 == 0 && dp[i] > dp[i / 2]) {
+				dp[i] = dp[i / 2];
 			}
-			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 3]);
+			if (i % 3 == 0 && dp[i] > dp[i / 3]) {
+				dp[i] = dp[i / 3];
 			}
 			dp[i]++;
 		}
