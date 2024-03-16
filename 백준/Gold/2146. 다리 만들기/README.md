7ae40376b27a701938c78884260d018924c99f12
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-03-17 00:28:22 +0900

    [Gold III] Title: 다리 만들기, Time: 352 ms, Memory: 86488 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/README.md
+++ 백준/Gold/2146. 다리 만들기/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 87812 KB, 시간: 368 ms
+메모리: 86488 KB, 시간: 352 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2024년 3월 16일 22:30:40
+2024년 3월 17일 00:28:11
 
 ### 문제 설명
 
@@ -20,11 +20,11 @@
 
 <p>이 나라는 N×N크기의 이차원 평면상에 존재한다. 이 나라는 여러 섬으로 이루어져 있으며, 섬이란 동서남북으로 육지가 붙어있는 덩어리를 말한다. 다음은 세 개의 섬으로 이루어진 나라의 지도이다.</p>
 
-<p style="text-align: center;"><img alt="" height="225" src="https://www.acmicpc.net/JudgeOnline/upload/201008/bri.PNG" width="243"></p>
+<p style="text-align: center;"><img alt="" height="225" src="" width="243"></p>
 
 <p>위의 그림에서 색이 있는 부분이 육지이고, 색이 없는 부분이 바다이다. 이 바다에 가장 짧은 다리를 놓아 두 대륙을 연결하고자 한다. 가장 짧은 다리란, 다리가 격자에서 차지하는 칸의 수가 가장 작은 다리를 말한다. 다음 그림에서 두 대륙을 연결하는 다리를 볼 수 있다.</p>
 
-<p style="text-align: center;"><img alt="" height="220" src="https://www.acmicpc.net/JudgeOnline/upload/201008/b2.PNG" width="247"></p>
+<p style="text-align: center;"><img alt="" height="220" src="" width="247"></p>
 
 <p>물론 위의 방법 외에도 다리를 놓는 방법이 여러 가지 있으나, 위의 경우가 놓는 다리의 길이가 3으로 가장 짧다(물론 길이가 3인 다른 다리를 놓을 수 있는 방법도 몇 가지 있다).</p>
 
--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Main {
 	static int[] dr = { 1, -1, 0, 0 }, dc = { 0, 0, 1, -1 };
 	static int N, map[][], id = 2;
+	static boolean[][] visited;
+	static Queue<Record> q;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,53 +15,41 @@ public class Main {
 				map[i][j] = sc.nextInt();
 			}
 		}
-
-		Map<Integer, boolean[][]> visitedMap = new HashMap<>();
-		Queue<Record> q = new LinkedList<>();
+		q = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] == 0) {
-					continue;
-				}
 				if (map[i][j] == 1) {
+					visited = new boolean[N][N];
 					dfs(i, j);
 					id++;
 				}
-				for (int k = 0; k < 4; k++) {
-					int nr = i + dr[k], nc = j + dc[k];
-					boolean[][] visited = visitedMap.get(map[i][j]);
-					if (visited == null) {
-						visited = new boolean[N][N];
-						visitedMap.put(map[i][j], visited);
-					}
-					if (isVisitable(nr, nc, visited) && map[nr][nc] == 0) {
-						q.add(new Record(nr, nc, 1, map[i][j]));
-						visited[nr][nc] = true;
-					}
-				}
-
 			}
 		}
 		while (!q.isEmpty()) {
 			Record cur = q.poll();
 			for (int i = 0; i < 4; i++) {
 				int nr = cur.r + dr[i], nc = cur.c + dc[i];
-				boolean[][] visited = visitedMap.get(cur.id);
-				if (!isVisitable(nr, nc, visited)) {
+				if (!cur.isVisitable(nr, nc)) {
 					continue;
-				} else if (map[nr][nc] == 0) {
-					q.add(new Record(nr, nc, cur.dist + 1, cur.id));
-					visited[nr][nc] = true;
-				} else if (map[nr][nc] != cur.id) {
+				}
+				if (map[nr][nc] > 0) {
 					System.out.println(cur.dist);
 					return;
 				}
+				cur.visited[nr][nc] = true;
+				q.add(new Record(nr, nc, cur.dist + 1, cur.visited));
 			}
 		}
 	}
 
 	static void dfs(int r, int c) {
-		if (r >= 0 && r < N && c >= 0 && c < N && map[r][c] == 1) {
+		if (!isInRange(r, c)) {
+			return;
+		}
+		visited[r][c] = true;
+		if (map[r][c] == 0) {
+			q.add(new Record(r, c, 1, visited));
+		} else if (map[r][c] == 1) {
 			map[r][c] = id;
 			for (int i = 0; i < 4; i++) {
 				dfs(r + dr[i], c + dc[i]);
@@ -67,18 +57,23 @@ public class Main {
 		}
 	}
 
-	static boolean isVisitable(int nr, int nc, boolean[][] visited) {
-		return (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]);
+	static boolean isInRange(int r, int c) {
+		return (r >= 0 && r < N && c >= 0 && c < N);
 	}
 
 	static class Record {
-		int r, c, dist, id;
+		int r, c, dist;
+		boolean[][] visited;
 
-		Record(int r, int c, int dist, int id) {
+		Record(int r, int c, int dist, boolean[][] visited) {
 			this.r = r;
 			this.c = c;
 			this.dist = dist;
-			this.id = id;
+			this.visited = visited;
+		}
+
+		boolean isVisitable(int r, int c) {
+			return (isInRange(r, c) && !this.visited[r][c]);
 		}
 	}
 }
