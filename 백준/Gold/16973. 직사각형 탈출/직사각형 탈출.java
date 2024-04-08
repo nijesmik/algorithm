860a5dd566f1e19e1f5c94120ebdcb17b52b5cb8
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-04-08 13:30:47 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 1920 ms, Memory: 310672 KB -BaekjoonHub

--- 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
+++ 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
@@ -1,19 +1,24 @@
 import java.util.*;
 
 class Main {
-
     static int boardR, boardC, board[][], height, width, startR, startC, endR, endC;
-    static int[] dr = {1, -1, 0, 0}, dc = {0, 0, 1, -1};
+    static int[] dr = { 1, -1, 0, 0 }, dc = { 0, 0, 1, -1 };
     static boolean[][] visited;
+    static List<Point> walls;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boardR = sc.nextInt();
         boardC = sc.nextInt();
         board = new int[boardR][boardC];
+        walls = new ArrayList<>();
         for (int i = 0; i < boardR; i++) {
             for (int j = 0; j < boardC; j++) {
-                board[i][j] = sc.nextInt();
+                int item = sc.nextInt();
+                board[i][j] = item;
+                if (item == 1) {
+                    walls.add(new Point(i, j));
+                }
             }
         }
         height = sc.nextInt();
@@ -46,29 +51,33 @@ class Main {
     }
 
     static boolean canGo(int r, int c) {
-        if (r < 0 || c < 0 || r + height - 1 >= boardR || c + width - 1 >= boardC
-            || visited[r][c]) {
+        if (r < 0 || c < 0 || r + height - 1 >= boardR || c + width - 1 >= boardC || visited[r][c]) {
             return false;
         }
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (board[r + i][c + j] == 1) {
-                    return false;
-                }
+        for (Point wall : walls) {
+            if (wall.r >= r && wall.r < r + height && wall.c >= c && wall.c < c + width) {
+                return false;
             }
         }
         return true;
     }
 
-    static class Record {
+    static class Record extends Point {
+        int time;
 
-        int r, c, time;
-
-        public Record(int r, int c, int time) {
+        Record(int r, int c, int time) {
+            super(r, c);
             visited[r][c] = true;
+            this.time = time;
+        }
+    }
+
+    static class Point {
+        int r, c;
+
+        Point(int r, int c) {
             this.r = r;
             this.c = c;
-            this.time = time;
         }
     }
 }
