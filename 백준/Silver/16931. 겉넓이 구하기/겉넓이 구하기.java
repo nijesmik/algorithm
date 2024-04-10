60077aac83b3c5d87cd77c3da47b510c32d21e24
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-04-10 22:04:06 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 256 ms, Memory: 25368 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
@@ -1,36 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    static int boardC, total;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int boardR = sc.nextInt();
-        boardC = sc.nextInt();
+        int boardR = sc.nextInt(), boardC = sc.nextInt(), total = 0;
         int[][] board = new int[boardR + 2][boardC + 2];
-        total = 0;
         for (int i = 1; i <= boardR; i++) {
             for (int j = 1; j <= boardC; j++) {
                 board[i][j] = sc.nextInt();
                 if (board[i][j] > 0) {
-                    total += 2; // top and bottom
+                    total += 2;
                 }
-                total += Math.max(0, board[i][j] - board[i][j - 1]); // left
             }
-            right(board[i]);
         }
-        for (int j = 1; j <= boardC; j++) {
-            for (int i = 1; i <= boardR; i++) {
-                total += Math.max(0, board[i][j] - board[i - 1][j]) // back
-                        + Math.max(0, board[boardR - i + 1][j] - board[boardR - i + 2][j]); // front
+        for (int i = 1; i <= boardR; i++) {
+            for (int j = 1; j <= boardC; j++) {
+                total += Math.max(0, board[i][j] - board[i][j - 1])
+                        + Math.max(0, board[i][boardC - j + 1] - board[i][boardC - j + 2])
+                        + Math.max(0, board[i][j] - board[i - 1][j])
+                        + Math.max(0, board[boardR - i + 1][j] - board[boardR - i + 2][j]);
             }
         }
         System.out.println(total);
-    }
-
-    static void right(int[] row) {
-        for (int i = boardC; i > 0; i--) {
-            total += Math.max(0, row[i] - row[i + 1]);
-        }
     }
 }
