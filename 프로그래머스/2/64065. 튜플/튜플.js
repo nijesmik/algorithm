50e5c4dd1e95422e5d6cf64c9239e1d01b4d361f
kimsejin <128948538+nijesmik@users.noreply.github.com>
2024-07-14 16:52:47 +0900

    [level 2] Title: 튜플, Time: 30.95 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,20 +1,12 @@
 function solution(s) {
-    const subsets = s.substring(2, s.length - 2).split('},{');
-    const answer = [];
-    subsets.sort(compare).forEach(subset => {
-        const set = new Set(subset.split(','));
-        answer.forEach(e => set.delete(e));
-        answer.push(...set);
-    })
-    return answer.map(Number);
-}
-
-const compare = (a, b) => {
-    if (a.length < b.length) {
-        return -1;
-    }
-    if (a.length > b.length) {
-        return 1;
-    }
-    return 0;
+  return s
+    .slice(2, -2)
+    .split('},{')
+    .sort((a, b) => a.length - b.length)
+    .reduce((answer, subset) => {
+      const set = new Set(subset.split(','));
+      answer.forEach((e) => set.delete(e));
+      return [...answer, ...set];
+    }, [])
+    .map(Number);
 }
