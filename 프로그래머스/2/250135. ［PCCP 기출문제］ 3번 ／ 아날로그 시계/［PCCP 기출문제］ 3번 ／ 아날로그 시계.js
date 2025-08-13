ab8c0dcc5e5c00c05de75b7f497f62836dd47996
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-13 20:39:24 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 24.20 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.js
@@ -0,0 +1,69 @@
+const getTime = (h, m ,s) => {
+    return (s + m * 60 + h * 60 * 60);
+}
+
+const compareTime = ([h1, m1, s1], [h2, m2, s2]) => {
+    const time1 = getTime(h1, m1, s1);
+    const time2 = getTime(h2, m2, s2);
+    
+    if (time1 < time2) {
+        return -1;
+    } else if (time1 == time2) {
+        return 0;
+    }
+    return 1;
+}
+
+const increaseTime = (time) => {
+    time[2]++;
+    
+    time[1] += Math.floor(time[2] / 60);
+    time[2] = time[2] % 60;
+    
+    time[0] += Math.floor(time[1] / 60);
+    time[1] = time[1] % 60;
+    
+    return time;
+}
+
+const getPositionHour = ([h, m, s]) => getTime(h, m, s) / 3600 * 5;
+
+const getPositionMinute = ([h, m, s]) => m + s / 60;
+
+function solution(h1, m1, s1, h2, m2, s2) {
+    const start = [h1, m1, s1];
+    const end = [h2, m2, s2];
+    
+    let answer = 0;
+    let h = h1;
+    let m = m1;
+    while (compareTime([h, m, 0], end) <= 0) {
+        let match = 0;
+        
+        // s = m + s / 60
+        // 60s = 60m + s
+        const time1 = [h, m, 60 * m / 59];
+        if (time1[2] < 60 && compareTime(start, time1) <= 0 && compareTime(time1, end) <= 0) {
+            match++;
+        }
+        
+        const h2 = h % 12;
+        // s = (h + m / 60 + s / 3600) * 5
+        // 3600s = (3600h + 60s) * 5 + 5s
+        const time2 = [h, m, (3600 * h2 + 60 * m) * 5 / 3595];
+        if (time2[2] < 60 && compareTime(start, time2) <= 0 && compareTime(time2, end) <= 0) {
+            match++;
+        }
+        
+        if (match == 2 && time1[2] === time2[2]) {
+            match = 1;
+        }
+        answer += match;
+        
+        m++;
+        h += Math.floor(m / 60);
+        m %= 60;
+    }
+    
+    return answer;
+}
