86f3c44f27cde852035e37ef4fde8b60d98dddcd
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-18 16:25:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -0,0 +1,38 @@
+const parseTime = (time) => {
+    const [mm, ss] = time.split(":").map(Number);
+    
+    return mm * 60 + ss;
+}
+
+function solution(video_len, pos, op_start, op_end, commands) {
+    const video_time = parseTime(video_len);
+    const op_start_time = parseTime(op_start);
+    const op_end_time = parseTime(op_end);
+    
+    const isOpening = (time) => op_start_time <= time && time <= op_end_time;
+    
+    const moved_time = commands.reduce((time, cmd) => {
+        if (isOpening(time)) {
+            time = op_end_time;
+        }
+        
+        if (cmd === "prev") {
+            const current = time - 10;
+            return current < 0 ? 0 : current;
+        }
+        
+        // cmd === "next"
+        const current = time + 10;
+        return current > video_time ? video_time : current;
+    }, parseTime(pos));
+    
+    if (isOpening(moved_time)) {
+        return op_end;
+    }
+    
+    const m = Math.floor(moved_time / 60);
+    const m_str = m < 10 ? `0${m}` : m;
+    const s = moved_time % 60;
+    const s_str = s < 10 ? `0${s}` : s;
+    return `${m_str}:${s_str}`;
+}
