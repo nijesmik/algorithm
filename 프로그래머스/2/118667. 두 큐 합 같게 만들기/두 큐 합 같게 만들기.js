987c17c23dcdad9c58d97ab15bcfc72ab48faf5e
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-19 22:56:59 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 25.94 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,38 +1,24 @@
-const answer = (start, end, size) => {
-    return end < size ? -1 : start + end - size;
-}
-
-const find = (queue, target) => {
-    let start = 0;
-    let end = 0;
-    let sum = queue[0];
-    
-    while (start < queue.length) {
-        if (sum === target) {
-            console.log(start, end)
-            return answer(start, end, Math.floor(queue.length / 2) - 1);
-        } else if (sum < target) {
-            end++;
-            if (end >= queue.length) {
-                return -1;
-            }
-            sum += queue[end];
-        } else {
-            sum -= queue[start];
-            start++;
-        }
-    }
-    
-    return -1;
-}
-
 function solution(queue1, queue2) {
-    const sum1 = queue1.reduce((s, n) => s + n, 0);
-    const sum2 = queue2.reduce((s, n) => s + n, 0);
-    const target = Math.floor((sum1 + sum2) / 2);
+    const N = queue1.length * 2;
+    let sum1 = queue1.reduce((s, n) => s + n, 0);
+    let sum2 = queue2.reduce((s, n) => s + n, 0);
     
-    const answer1 = find([...queue1, ...queue2], target);
-    const answer2 = find([...queue2, ...queue1], target);
-    
-    return answer1 == -1 || answer2 == -1 ? Math.max(answer1, answer2) : Math.min(answer1, answer2)
+    let count = 0, index1 = 0, index2 = 0;
+    while (sum1 !== sum2) {
+        if (sum1 < sum2) {
+            if (index2 >= N) return -1;
+            const popped = queue2[index2++];
+            queue1.push(popped);
+            sum1 += popped;
+            sum2 -= popped;
+        } else {
+            if (index1 >= N) return -1;
+            const popped = queue1[index1++];
+            queue2.push(popped);
+            sum2 += popped;
+            sum1 -= popped;
+        }
+        count++;
+    }
+    return count;   
 }
