9f42bcd2e94c119a2e69decdfba52f9c334a63af
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-21 18:46:24 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 27.15 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -2,23 +2,16 @@ function solution(friends, gifts) {
     const N = friends.length;
     const indexMap = new Map(friends.map((name, index) => [name, index]));
     const point = Array.from({length: N}, () => new Array(N).fill(0));
+    const pointSum = new Array(N).fill(0);
     
     gifts.forEach((gift) => {
         const [from, to] = gift.split(" ").map((name) => indexMap.get(name));
         point[from][to]++;
+        pointSum[from]++;
+        pointSum[to]--;
     })
     
-    const pointSum = point
-            .map((sent) => sent.reduce((a, c) => a + c), 0)
-            .map((sum_sent, index) => {
-                let sum_received = 0;
-                for (let j = 0; j < N; j++) {
-                    sum_received += point[j][index];
-                }
-                return sum_sent - sum_received;
-            });
-    
-    return point.map((sent, i) => sent.reduce((expected, point_sent, j) => {
+    const expect = point.map((sent, i) => sent.reduce((expected, point_sent, j) => {
         const pointDiff = point_sent - point[j][i];
         const pointSumDiff = pointSum[i] - pointSum[j];
         
@@ -26,5 +19,7 @@ function solution(friends, gifts) {
             return expected + 1;
         }
         return expected;
-    }, 0)).reduce((ans, e) => Math.max(ans, e), 0);
+    }, 0));
+    
+    return Math.max(...expect);
 }
