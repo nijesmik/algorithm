1c995e42e2babfc759ca1b772cd12278d118a2a0
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-27 23:27:36 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 441.32 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,17 +1,14 @@
 function solution(numbers) {
-    return numbers
-        .map((num) => ['0', ...num.toString(2)])
-        .map((num) => {
-            for (let i = num.length - 1; i >= 0; i--) {
-                if (num[i] === '0') {
-                    num[i] = '1';
-                    break;
-                } else if (num[i] === '1' && i > 0 && num[i - 1] === '0') {
-                    num[i] = '0';
-                    num[i - 1] = '1';
-                    break;
-                }
-            }
-            return parseInt(num.join(''), 2);
-        })
+    return numbers.map((num) => {
+        const n = BigInt(num);
+        if (n % 2n === 0n) {
+            return n + 1n;
+        }
+
+        let bit = 1n;
+        while ((n & bit) > 0n) {
+            bit <<= 1n;
+        }
+        return n + bit - (bit >> 1n);
+    })
 }
