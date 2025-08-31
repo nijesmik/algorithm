938876ccab6b308e06d3f86050ce2af17c2ab348
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-08-31 13:43:03 +0900

    [level 2] Title: 순위 검색, Time: 659.37 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.js
@@ -0,0 +1,55 @@
+function solution(info, query) {
+    const map = new Map();
+    
+    const addScore = (key, score) => {
+        if (!map.has(key)) {
+            map.set(key, [0]);
+        }
+        map.get(key).push(score);
+    }
+    
+    const dfs = (depth, types, score) => {
+        if (depth === 4) {
+            addScore(types.join(''), score);
+            return;
+        }
+        
+        dfs(depth + 1, types, score);
+        const t = [...types];
+        t[depth] = '-';
+        dfs(depth + 1, t, score);
+    }
+    
+    info.forEach((applicant) => {
+        const types = applicant.split(" ");
+        const score = Number(types.pop());
+        dfs(0, types, score);        
+    });
+
+    for (const scores of map.values()) {
+        scores.sort((a, b) => a - b);
+    }
+    
+    const bs = (scores, target) => {
+        let start = 0;
+        let end = scores.length - 1;
+        
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (scores[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return scores.length - 1 - end;
+    }
+    
+    return query.map((q) => {
+        const types = q.replace(/and\s/g, "").split(" ");
+        const target = Number(types.pop());
+        const scores = map.get(types.join('')) ?? [];
+        return bs(scores, target);
+    });
+}
