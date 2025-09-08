2a7e04cb21d755d50823b49f8438fbc22e4a628a
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-09 03:21:31 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 64.39 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,18 +1,16 @@
 function solution(elements) {
-    const seq = [...elements, ...elements];
+    const seq = elements.concat(elements);
     const set = new Set(elements);
     const acc = elements.reduce((acc, elem, i) => {
-        const prev = acc[i - 1] ?? 0;
-        acc[i] = elem + prev;
+        acc[i + 1] = elem + acc[i];
         return acc;
-    }, [])
+    }, [0])
     
     for (let len = 2; len <= elements.length; len++) {
-        let sum = acc[len - 1];
+        let sum = acc[len];
         set.add(sum);
         for (let idx = 0; idx < elements.length; idx++) {
-            sum -= seq[idx];
-            sum += seq[idx + len];
+            sum = sum - seq[idx] + seq[idx + len];
             set.add(sum);
         }
     }
