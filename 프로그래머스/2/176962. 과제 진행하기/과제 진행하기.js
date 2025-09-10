59ac1734531b0ab4b508974883f19e0e1832c99b
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-11 01:50:24 +0900

    [level 2] Title: 과제 진행하기, Time: 49.96 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -0,0 +1,27 @@
+function solution(plans) {
+    plans = plans.map(([name, start, take]) => {
+        const [hh, mm] = start.split(":").map(Number);
+        return [name, hh * 60 + mm, Number(take)];
+    }).sort((a, b) => a[1] - b[1])
+    
+    const answer = [];
+    const wip = [];
+    
+    for (const [curr, start, take] of plans) {
+        while (wip.length > 0 && wip[wip.length - 1][1] <= start) {
+            const [done] = wip.pop();
+            answer.push(done);
+        }
+        
+        wip.forEach((_, i) => wip[i][1] += take);
+        wip.push([curr, start + take]);
+        wip.sort((a, b) => b[1] - a[1]);
+    }
+    
+    let n = wip.length;
+    while (n-- > 0) {
+        answer.push(wip[n][0]);
+    }
+    
+    return answer;
+}
