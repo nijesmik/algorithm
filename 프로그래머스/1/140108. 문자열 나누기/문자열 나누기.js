9c35ac042f1ddf72fa12629f2cbc3ad4d08cb5fb
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-12 23:44:37 +0900

    [level 1] Title: 문자열 나누기, Time: 3.95 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,19 +1,16 @@
 function solution(s) {
     let x = s[0];
-    let words = -1;
-    let making = [];
+    let words = 0;
     const count = [0, 0];
     
     for (let char of s) {
         if (count[0] === count[1]) {
             words++;
-            making = [];
             x = char;
         }
         
-        making.push(char);
         count[Number(x === char)]++;                
     }
     
-    return words + (making.length ? 1 : 0);
+    return words;
 }
