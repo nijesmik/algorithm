808cfb15dded8405f3bc4e425f225aa9b5556968
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-16 20:44:32 +0900

    [level 1] Title: 명예의 전당 (1), Time: 315.78 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,21 +1,12 @@
 function solution(k, score) {
-    const answer = [];
     const honor = [];
     
-    const peek = () => honor[honor.length - 1];
-    
-    const push = (score) => {
-        honor.push(score);
+    return score.map((today) => {
+        honor.push(today);
         honor.sort((a, b) => b - a);
         while (honor.length > k) {
             honor.pop();
         }
-    }
-    
-    for (const today of score) {
-        push(today);
-        answer.push(peek());
-    }
-    
-    return answer;
+        return honor[honor.length - 1];
+    })
 }
