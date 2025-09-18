7285fe2512b5c78a0e280022042873a77f6c809d
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-18 21:30:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 127.56 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,45 +1,24 @@
-const getPrimeList = (n) => {
-    const isPrime = new Array(n + 1).fill(true);
-    
-    isPrime[0] = false;
-    isPrime[1] = false;
-    
-    for (let i = 2; i * i <= n; i++) {
-        if (!isPrime[i]) {
-            continue;
+function solution(number, limit, power) {
+    const getPower = (n) => {
+        let count = 0;
+        for (let i = 1; i * i <= n; i++) {
+            if (count > limit) {
+                return power;
+            }
+            if (n % i === 0) {
+                count += 2;
+            }
+            if (i * i === n) {
+                count -= 1;
+            }
         }
-        for (let j = i * 2; j <= n; j += i) {
-            isPrime[j] = false;
-        }
+        return count > limit ? power : count;
     }
     
-    return isPrime.reduce((prime, bool, num) => {
-        if (bool) {
-            prime.push(num);
-        }
-        return prime;
-    }, []);
-}
-
-function solution(number, limit, power) {
-    const primeList = getPrimeList(number);
+    let answer = 1;
+    for (let i = 2; i <= number; i++) {
+        answer += getPower(i);
+    }
     
-    return Array.from({length: number}, (_, i) => {
-        let num = i + 1;
-        let total = 1;
-        for (const prime of primeList) {
-            if (prime > num) {
-                return total;
-            }
-            
-            let count = 0;
-            while (num % prime === 0) {
-                num = Math.floor(num / prime);
-                count++;
-            }
-            total *= count + 1;
-        }
-        
-        return total;
-    }).reduce((acc, divisor) => acc + (divisor > limit ? power : divisor));
+    return answer;
 }
