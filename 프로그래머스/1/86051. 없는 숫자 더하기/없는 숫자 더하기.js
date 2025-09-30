da7217267f869f67a6202106fe176260906ced65
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-09-30 15:25:32 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,14 +1,3 @@
 function solution(numbers) {
-    const exist = new Array(10).fill(false);
-    
-    numbers.forEach((n) => {
-        exist[n] = true;
-    });
-    
-    return exist.reduce((sum, isExist, n) => {
-        if (!isExist) {
-            sum += n;
-        }
-        return sum;
-    }, 0);
+    return 45 - numbers.reduce((sum, num) => sum + num);
 }
