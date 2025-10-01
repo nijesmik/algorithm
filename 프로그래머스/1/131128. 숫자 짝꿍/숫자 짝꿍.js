72ae75f4196822c5bfab175867eea26a0fd057e0
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-10-01 16:47:45 +0900

    [level 1] Title: 숫자 짝꿍, Time: 369.90 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,25 @@
+function solution(X, Y) {
+    const countX = new Array(10).fill(0);
+    const countY = new Array(10).fill(0);
+
+    Array.from(X).map(Number).forEach((x) => countX[x]++);
+    Array.from(Y).map(Number).forEach((y) => countY[y]++);
+    
+    const result = [];
+    for (let i = 9; i >= 0; i--) {
+        const pair = Math.min(countX[i], countY[i]);
+        if (pair) {
+            result.push(i.toString().repeat(pair));   
+        }
+    }
+    
+    if (!result.length) {
+        return '-1';        
+    }
+
+    if (result.length === 1 && result[0][0] === '0') {
+        return '0';
+    }
+
+    return result.join('');
+}
