8aaef04c3068fe2e148d0023cd67b78386489a2b
김세진 <128948538+nijesmik@users.noreply.github.com>
2025-10-08 18:29:11 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,14 +1,11 @@
 function solution(new_id) {
-    let tmp_id = new_id
+    const tmp_id = new_id
         .toLowerCase()
-        .replace(/[^a-z0-9\-_.]/g, '')
-        .replace(/[.]{2,}/g, '.')
-        .replace(/^\.|\.$/g, '');
-    
-    tmp_id = !tmp_id ? 'a' : tmp_id.slice(0, 15).replace(/^\.|\.$/g, '');
+        .replace(/[^\w\-_.]/g, '')
+        .replace(/\.+/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .padEnd(1, 'a')
+        .slice(0, 15).replace(/^\.|\.$/g, '');
 
-    if (tmp_id.length < 3) {
-        return tmp_id.padEnd(3, tmp_id.slice(-1));
-    }
-    return tmp_id;
+    return tmp_id.padEnd(3, tmp_id.slice(-1));
 }
